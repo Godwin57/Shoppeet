406b7f3d7fc3bcd53982720defe8edfe11655eb0
Godwin Emeribe <godwinemeribe23@gmail.com>
2024-01-18 15:35:21 +0100

    Creating a separate page for categories

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,26 @@
+import fetchData from "@/utils/fetchData";
+import {productType} from "@/types/fetchProductsType";
+
+const getUniqueElem = (arr:string[]) => {
+    let uniqueElem = new Set(arr);
+    return Array.from(uniqueElem);
+}
+
+async function CateogoriesPage() {
+    const URL = 'http://localhost:3000/products';
+    const products = await fetchData(URL);
+
+    const categories = getUniqueElem(products.map((product:productType) => product.category));
+
+
+    return (
+        <div>
+            <h1>Available categories:</h1>
+            {categories.map((category:string) => (
+                <h2 key={category}>{category}</h2>
+            ))}
+        </div>
+    );
+}
+
+export default CateogoriesPage;
